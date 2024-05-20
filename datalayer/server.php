50c21f566c89c49f0640f6bb0187c8051a9a359e
Gunther <kipyegokevin82@gmail.com>
2024-05-20 09:33:16 +0300

    added the zesport files

--- datalayer/server.php
+++ datalayer/server.php
@@ -1,15 +1,18 @@
+
 <?php
 
-$host = 'localhost';
-$username = 'root';
-$password = 'kevo123';
-$database = 'zesportdb';
+// Load environment variables from .env file
+$dotenv = parse_ini_file('../.env');
+
+$host = $dotenv['host'];
+$username = $dotenv['username'];
+$password = $dotenv['password'];
+$database = $dotenv['database'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    
 } catch (PDOException $e) {
     die("Database connection or thumbnail generation error: " . $e->getMessage());
 }
